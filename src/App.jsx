230979041ad00280d24268a2d8b59515cb9d66e8
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2024-01-18 23:30:17 +0100

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Auth from './components/Auth.jsx'
+import EmailConfirmation from './components/EmailConfirmation.jsx';
+import AccountActivation from './components/AccountActivation.jsx';
+import Dashboard from './components/Dashboard.jsx';
+import { ListWrapper } from './components/ListWrapper.jsx';
 
 export default function App() {
     return (
-        <div>
-            <Auth/>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/email-confirmation" element={<EmailConfirmation />} />
+                <Route path="/activateAccount" element = {<AccountActivation />} />
+                <Route path="/dashboard" element = {<Dashboard />} />
+                <Route path="/list" element = {<ListWrapper />} />
+            </Routes>
+        </Router>
     )
 }
