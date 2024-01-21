d8123397aac9a991ee1c4b3f8cd8dc3890359301
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2024-01-22 00:43:42 +0100

    Added all functionalities to the dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,10 +2,18 @@ import "../styles/dashboard.css";
 import React, { useEffect, useState } from "react";
 import { EntryListWrapper } from "./EntryListWrapper";
 import { ReminderListWrapper } from "./ReminderListWrapper";
+import { TreatmentProcessInfo } from "./TreatmentProcessInfo";
 import Cookies from "js-cookie";
 import { Link } from 'react-router-dom';
+import { jwtDecode } from "jwt-decode";
 
 const Dashboard = () => {
+
+    const token = Cookies.get('token');
+    var decodedToken = jwtDecode(token);
+    var userName = decodedToken[
+        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"
+    ];
 
     const logout = (e) => {
         Cookies.remove('token');
@@ -14,13 +22,18 @@ const Dashboard = () => {
     return (
         <div className="dashboard-mainwrapper">
             <div className="dashboard-header">
-                <p className="dashboard-myaccount">Moje konto</p>
+                <p className="dashboard-myaccount">Cześć, {userName}!</p>
                 <Link to="/auth">
                     <p className="dashboard-logout" onClick={logout}>Wyloguj się</p>
                 </Link>
             </div>
+            <div className="dashboard-left">
             <EntryListWrapper></EntryListWrapper>
+            </div>
+            <div className="dashboard-center">
             <ReminderListWrapper></ReminderListWrapper>
+            </div>
+            <div className="dashboard-right"><TreatmentProcessInfo></TreatmentProcessInfo></div>                         
         </div>
     );
 };
