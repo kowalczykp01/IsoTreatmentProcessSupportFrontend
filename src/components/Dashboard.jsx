f985779582519e070ba1525c654db0ab00a0021c
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2024-01-21 17:21:11 +0100

    Added logout action

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,22 @@
 import "../styles/dashboard.css";
 import React, { useEffect, useState } from "react";
 import { ListWrapper } from "./ListWrapper";
+import Cookies from "js-cookie";
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
+
+    const logout = (e) => {
+        Cookies.remove('token');               
+   }
+
     return (
         <div className="dashboard-mainwrapper">
             <div className="dashboard-header">
                 <p className="dashboard-myaccount">Moje konto</p>
-                <p className="dashboard-logout">Wyloguj się</p>
+                <Link to="/auth">
+                    <p className="dashboard-logout" onClick={logout}>Wyloguj się</p>
+                </Link>                
             </div>
             <ListWrapper></ListWrapper>
         </div>
