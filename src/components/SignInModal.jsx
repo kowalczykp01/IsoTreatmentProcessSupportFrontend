218797ea8aebdaea09d6760b9aabbd99c2e116ce
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2024-01-21 22:34:00 +0100

    Redundant library removed

--- src/components/SignInModal.jsx
+++ src/components/SignInModal.jsx
@@ -2,7 +2,6 @@ import "../styles/signinmodal.css";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
-import { jwtDecode } from "jwt-decode";
 
 export const SignInModal = ({ onClose }) => {
     const navigate = useNavigate();
