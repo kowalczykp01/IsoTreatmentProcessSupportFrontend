4256544a892e3a1ec104403e984c2ed6e66bd906
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2024-02-05 21:59:30 +0100

    Changes in Auth component - mobile first approach, layout changes

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -4,8 +4,8 @@ import { SignInModal } from "./SignInModal.jsx"
 import { SignUpModal } from "./SignUpModal.jsx"
 
 export default function Auth() {
-    const [modalOpen, setSignInModalOpen] = useState(false);
-    const [modalOpen1, setSignUpModalOpen] = useState(false);
+    const [signInModalOpen, setSignInModalOpen] = useState(false);
+    const [signUpModalOpen, setSignUpModalOpen] = useState(false);
 
     const handleSignInCloseButtonClick = () => {
         setSignInModalOpen(false);
@@ -15,17 +15,29 @@ export default function Auth() {
         setSignUpModalOpen(false);
     };
 
+    const openSignUpModal = (e) => {
+        e.preventDefault();
+        setSignUpModalOpen(true)
+    };
+
+    const openSignInModal = (e) => {
+        e.preventDefault();
+        setSignInModalOpen(true)
+    };  
+
     return (
-        <div className="mainwrapper">
-            <h1 className="title">IsoSupport</h1>
-            <button className="signupButton" onClick={() => setSignUpModalOpen(true)}>Utwórz konto</button>
-            <p className="alreadyHaveAnAccountLabel">Masz już konto?</p>
-            <button className="signinButton" onClick={() => setSignInModalOpen(true)}>Zaloguj się</button>
-            <p className="footerText">Celem IsoSupport jest jedynie ułatwienie procesu leczenia izotretynoiną. Decyzje dotyczącą podjęcia leczenia i jego przebiegu podejmuje lekarz. Dzienna dawka leku, jak i sumaryczna dawka w mg/kg m. c. wprowadzane do aplikacjii powinny być ustalone przez lekarza prowadzącego.</p>
-            {modalOpen && (
+        <div>
+            <h1 className="auth-title">IsoSupport</h1>
+            <form className="auth-form">
+                <button className="auth-signupButton" onClick={openSignUpModal}><span className="auth-span">Utwórz konto</span></button>
+                <p className="auth-alreadyHaveAnAccountLabel">Masz już konto?</p>
+                <button className="auth-signinButton" onClick={openSignInModal}><span className="auth-span">Zaloguj się</span></button>                
+            </form>
+            <p className="auth-footerText">Celem IsoSupport jest jedynie ułatwienie procesu leczenia izotretynoiną. Decyzje dotyczącą podjęcia leczenia i jego przebiegu podejmuje lekarz. Dzienna dawka leku, jak i sumaryczna dawka w mg/kg m. c. wprowadzane do aplikacjii powinny być ustalone przez lekarza prowadzącego.</p>
+            {signInModalOpen && (
                 <SignInModal onClose={handleSignInCloseButtonClick} />
             )}
-            {modalOpen1 && (
+            {signUpModalOpen && (
                 <SignUpModal onClose={handleSignUpCloseButtonClick} />
             )}
         </div>
